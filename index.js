90271b37a110fa403cd76f729a8de7ff379fd96a
Sanesthesie <victorsanlopez@gmail.com>
2024-10-14 16:19:50 +0200

    feat : use import instead of require

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-require("dotenv").config();
+import dotenv from 'dotenv';
+dotenv.config()
+import cowsay from "cowsay";
 
-const cowsay = require("cowsay");
 
 console.log(cowsay.say({
     text : `Hello I'm ${process.env.MY_NAME} from ${process.env.MY_CAMPUS}!`,
     e : "oO",
     T : "U "
 }));
-
